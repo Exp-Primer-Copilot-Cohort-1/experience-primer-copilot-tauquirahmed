9d8575b0156ccc190ea0cc59b102a9e81b5eacc6
tauquirahmed <100079429+tauquirahmed@users.noreply.github.com>
2024-03-24 19:44:15 +0000

    Third commit corrected

--- comments.js
+++ comments.js
@@ -1,75 +1,27 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var comments = [
-  {
-    name:'张三',
-    message:'今天天气不错！',
-    dateTime:'2018-12-25'
-  },
-  {
-    name:'张三',
-    message:'今天天气不错！',
-    dateTime:'2018-12-25'
-  },
-  {
-    name:'张三',
-    message:'今天天气不错！',
-    dateTime:'2018-12-25'
-  },
-  {
-    name:'张三',
-    message:'今天天气不错！',
-    dateTime:'2018-12-25'
-  }
-];
-http.createServer(function(req,res){
-  //parse url
-  var parseObj = url.parse(req.url,true);
-  var pathname = parseObj.pathname;
-  if(pathname === '/'){
-    fs.readFile('./views/index.html',function(err,data){
-      if(err){
-        return res.end('404 not found');
-      }
-      var htmlStr = template.render(data.toString(),{
-        comments:comments
-      });
-      res.end(htmlStr);
-    });
-  }else if(pathname === '/post'){
-    fs.readFile('./views/post.html',function(err,data){
-      if(err){
-        return res.end('404 not found');
-      }
-      res.end(data);
-    });
-  }else if(pathname.indexOf('/public/') === 0){
-    fs.readFile('.' + pathname,function(err,data){
-      if(err){
-        return res.end('404 not found');
-      }
-      res.end(data);
-    });
-  }else if(pathname === '/pinglun'){
-    var comment = parseObj.query;
-    comment.dateTime = '2018-12-25';
-    comments.unshift(comment);
-    res.statusCode = 302;
-    res.setHeader('Location','/');
-    res.end();
-  }else{
-    fs.readFile('./views/404.html',function(err,data){
-      if(err){
-        return res.end('404 not found');
-      }
-      res.end(data);
-    });
-  }
-}).listen(3000,function(){
-  console.log('Server is running...');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+//read file
+let comments = JSON.parse(fs.readFileSync('comments.json'));
+
+//parse data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//get all comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
-```
-###
+
+//get comment by id
+app.get('/comments/:id', (req, res) => {
+    let comment = comments.find((comment) => comment.id === parseInt(req.params.id));
+    if (!comment) {
+        return res.status(404).send('The comment with the given ID was not found.');
+    }
+    res.send(comment);
+});
+
